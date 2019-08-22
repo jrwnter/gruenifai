b4d73b2b46061ee86ff73885f8ca3b2c23191763
Robin Winter <robin.winter@bayer.com>
2019-08-22 17:48:53 +0200

    one more depth level

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='gruenifai',
     version='0.1',
-    packages=['backend', 'backend.postgres'],
+    packages=['gruenifai','gruenifai.backend', 'gruenifai.backend.postgres'],
     url='',
     license='',
     author='Robin WInter',
