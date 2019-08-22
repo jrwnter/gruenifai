2286863be049190a185b59dc813973f187954654
Robin Winter <robin.winter@bayer.com>
2019-08-22 15:25:47 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='gruenifai',
+    version='0.1',
+    packages=['backend', 'backend.postgres'],
+    url='',
+    license='',
+    author='Robin WInter',
+    author_email='',
+    description=''
+)
